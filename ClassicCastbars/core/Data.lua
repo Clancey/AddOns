2dbb102706e05b67bfe1e0cc90a4c365563dfc7b
James Clancey <james.clancey@gmail.com>
2019-08-29 20:02:22 -0800

    updated

--- ClassicCastbars/core/Data.lua
+++ ClassicCastbars/core/Data.lua
@@ -1267,7 +1267,7 @@ local function BuildSpellNameToSpellIDTable()
     end
 end
 
-C_Timer.After(1, BuildSpellNameToSpellIDTable) -- run asap once the current call stack has executed
+C_Timer.After(0.1, BuildSpellNameToSpellIDTable) -- run asap once the current call stack has executed
 
 -- For channeled spells we need both the spell ID and cast time since
 -- GetSpellInfo doesn't return any cast time for channeled casts.
@@ -1276,7 +1276,7 @@ C_Timer.After(1, BuildSpellNameToSpellIDTable) -- run asap once the current call
 -- TODO: merge with main spell table and just store the cast time here as table value
 namespace.channeledSpells = {
     -- MISC
-    [GetSpellInfo(746)] = { 6, 746 },         -- First Aid
+    [GetSpellInfo(746)] = { 8, 746 },         -- First Aid
     [GetSpellInfo(13278)] = { 4, 13278 },     -- Gnomish Death Ray
     [GetSpellInfo(20577)] = { 10, 20577 },    -- Cannibalize
     [GetSpellInfo(10797)] = { 6, 10797 },     -- Starshards
